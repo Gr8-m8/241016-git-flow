7610fd491f9117ab4658c9b6045f89422ae91e42
M L <martinn.lindbladd@gmail.com>
2024-10-18 10:16:43 +0200

    releasecandidatemerge

---
+++ releasecandidateMergeTomain.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+DEBUG = True
+
+versionfilepath = "version.data"
+
+gitmergecommand = "git merge"
+gitswitchbranch = "git switch"
+gitgetbranchcommand = "git rev-parse --abbrev-ref HEAD"
+
+branchtype = "release"
+destinationbranchname = "main"
+currentbranchname = subprocess.getoutput(gitgetbranchcommand)
+
+if not currentbranchname.startswith(branchtype):
+    version: str
+    versionfile = open(versionfilepath, "r")
+    version = versionfile.readline()
+    versionfile.close()
+
+    output_gitswitchbranchcommand = f"{gitswitchbranch} {branchtype}_{version}"
+    print(output_gitswitchbranchcommand) if DEBUG else None
+    subprocess.call(output_gitswitchbranchcommand) if not DEBUG else None
+
+output_gitmergecommand = f"{gitmergecommand} {destinationbranchname}"
+print(output_gitmergecommand) if DEBUG else None
+subprocess.call(output_gitmergecommand) if not DEBUG else None
+
+
